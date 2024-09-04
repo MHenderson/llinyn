7653dcb7555b951f79943aca478fe4124f794868
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-09-04 18:37:29 +0000

    Add new repair_latex_string function.

--- R/repair-latex.R
+++ R/repair-latex.R
@@ -1,18 +1,29 @@
+#' Escape A Few Things
+#'
+#' Replace certain characters in a string with escaped versions of those same
+#' characters for the benefit of LaTeX.
+#'
+#' @param x A text strirng.
+#'
+#' @return A text string with some escaped characters.
+#' @export
+repair_latex_string <- function(x) {
+  x <- stringr::str_replace_all(text, "\\&", "\\\\&")
+  x <- stringr::str_replace_all(tex, "\\_", "\\\\_")
+  x <- stringr::str_replace_all(tex, "\\#", "")
+  x <- stringr::str_replace_all(tex, "\\$", "\\\\textdollar")
+  return(x)
+}
+
 #' Repair LaTeX
 #'
-#' @param X A dataframe.
+#' @param X A data frame with a text column.
 #'
-#' @return
+#' @return A new data frame containing a tex column.
 #' @export
-#'
-#' @examples
 repair_latex <- function(X) {
   X |>
     dplyr::mutate(
-      tex = stringr::str_replace_all(text, "\\&", "\\\\&"),
-      tex = stringr::str_replace_all(tex, "\\_", "\\\\_"),
-      tex = stringr::str_replace_all(tex, "\\#", ""),
-      # we really only should replace lone dollars, not those that denote equations
-      tex = stringr::str_replace_all(tex, "\\$", "\\\\textdollar")
+      tex = repair_latex_string(text)
     )
 }
