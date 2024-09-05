cc970e8074f7f27f06dc504b267d93cdf154200b
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-09-05 09:16:30 +0000

    Fix the repair_latex_string function.

--- R/repair-latex-string.R
+++ R/repair-latex-string.R
@@ -3,14 +3,14 @@
 #' Replace certain characters in a string with escaped versions of those same
 #' characters for the benefit of LaTeX.
 #'
-#' @param x A text strirng.
+#' @param x A text string.
 #'
 #' @return A text string with some escaped characters.
 #' @export
 repair_latex_string <- function(x) {
-  x <- stringr::str_replace_all(text, "\\&", "\\\\&")
-  x <- stringr::str_replace_all(tex, "\\_", "\\\\_")
-  x <- stringr::str_replace_all(tex, "\\#", "")
-  x <- stringr::str_replace_all(tex, "\\$", "\\\\textdollar")
+  x <- stringr::str_replace_all(x, "\\&", "\\\\&")
+  x <- stringr::str_replace_all(x, "\\_", "\\\\_")
+  x <- stringr::str_replace_all(x, "\\#", "")
+  x <- stringr::str_replace_all(x, "\\$", "\\\\textdollar")
   return(x)
 }
