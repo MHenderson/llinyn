60eaad921fdd33091d4533622141e0e9b3f8df01
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-09-08 14:46:29 +0100

    Prepare v0.2.0 release (#24)
    * Export strip_time_headings and ymd_text_format functions.
    
    * Remove all data frame functions.
    
    * Update dependencies and bump version number.
    
    * Update NEWS.md.
    
    * Update README.

--- R/strip-time-headings.R
+++ R/strip-time-headings.R
@@ -5,6 +5,7 @@
 #' @param text A string of text, possibly containing datetime headings.
 #'
 #' @return The input string with all datetime headings removed.
+#' @export
 strip_time_headings <- function(text) {
   text <- stringr::str_replace(text, "% [A-Za-z]+\\s+\\d+ [A-Za-z]+ \\d{2}:\\d{2}:\\d{2} (GMT|BST) \\d+\n\n", "")
   text <- stringr::str_replace(text, "% [A-Za-z]+\\s+[A-Za-z]+\\s+\\d+ \\d{2}:\\d{2}:\\d{2} (GMT|BST) \\d+\n\n", "")
